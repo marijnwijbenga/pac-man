94de707df410f3c194d240eaae0eb8940d692538
Marijn Wijbenga <marijn@wijbit.nl>
2020-08-02 15:45:12 +0200

    canvas drawing maze continuation

--- pacman.js
+++ pacman.js
@@ -1,5 +1,5 @@
 console.log('this works');
-const stroke = 32;
+const stroke = 20;
 const strokeColor = 'red'
 const canvas = document.getElementById('game');
 const canvasContext = canvas.getContext('2d');
@@ -7,6 +7,7 @@ const canvasContext = canvas.getContext('2d');
 window.onload = function() {
 
    drawMaze();
+   drawPacman();
 }
 
 function drawMaze() {
@@ -15,11 +16,14 @@ function drawMaze() {
 
     canvasContext.fillStyle = strokeColor;
     canvasContext.fillRect(0, 0, canvas.width, stroke);
-    canvasContext.fillRect(0, 0, stroke, 256);
-    canvasContext.fillRect(canvas.width-stroke, 0, stroke, 256);
-    canvasContext.fillRect(0, 256, 128, stroke);
-    canvasContext.fillRect(canvas.width-128, 256, 128, stroke);
 
 
 
+
+}
+
+function drawPacman() {
+    canvasContext.fillStyle = 'yellow';
+    canvasContext.arc(120, 50, 20, 0, 2 * Math.PI);;
+    canvasContext.fill();
 }
