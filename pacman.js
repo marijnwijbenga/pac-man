7824ee35d08ce792c80a8c021fab258c8ad244af
Marijn Wijbenga <marijn@wijbit.nl>
2020-08-02 12:13:17 +0200

    added bg image and drawing the canvas

--- pacman.js
+++ pacman.js
@@ -1,5 +1,6 @@
 console.log('this works');
-const line = 16;
+const stroke = 32;
+const strokeColor = 'red'
 const canvas = document.getElementById('game');
 const canvasContext = canvas.getContext('2d');
 
@@ -10,12 +11,15 @@ window.onload = function() {
 
 function drawMaze() {
     canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    // canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
-    canvasContext.fillStyle = 'blue';
-    canvasContext.fillRect(0, 0, canvas.width, line);
-    canvasContext.fillRect(0, 0, line, 128);
-    canvasContext.fillRect(canvas.width-line, 0, line, 128);
+    canvasContext.fillStyle = strokeColor;
+    canvasContext.fillRect(0, 0, canvas.width, stroke);
+    canvasContext.fillRect(0, 0, stroke, 256);
+    canvasContext.fillRect(canvas.width-stroke, 0, stroke, 256);
+    canvasContext.fillRect(0, 256, 128, stroke);
+    canvasContext.fillRect(canvas.width-128, 256, 128, stroke);
+
 
 
 }
