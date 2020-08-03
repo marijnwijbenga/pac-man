0fb0407737b32d6e680292be6958e68b96de3260
Marijn Wijbenga <marijn@wijbit.nl>
2020-08-03 10:17:01 +0200

    drawing the maze on the canvas

--- pacman.js
+++ pacman.js
@@ -15,15 +15,40 @@ function drawMaze() {
     // canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
     canvasContext.fillStyle = strokeColor;
+
+    // outer lines
     canvasContext.fillRect(0, 0, canvas.width, stroke);
+    canvasContext.fillRect(0, 0, stroke, 240 );
+    canvasContext.fillRect(0, 0, stroke, 240 );
+
+    // blocks left top
+    canvasContext.fillRect(80, 80, 80, stroke );
+    canvasContext.fillRect(240, 80, 80, stroke );
+    canvasContext.fillRect(80, 160, 80, stroke );
+    canvasContext.fillRect(240, 160, stroke, 160 );
+    canvasContext.fillRect(240, 230, 80, stroke );
+    canvasContext.fillRect(240, 230, 80, stroke );
 
 
+    // canvasContext.fillRect();
+
+    // center line
+    canvasContext.fillRect(390, 0, stroke, 800 );
 
 
 }
 
 function drawPacman() {
     canvasContext.fillStyle = 'yellow';
-    canvasContext.arc(120, 50, 20, 0, 2 * Math.PI);;
+    canvasContext.arc(50, 90, 20, 0, 2 * Math.PI);
+    canvasContext.arc(360, 90, 20, 0, 2 * Math.PI);
+    canvasContext.arc(200, 90, 20, 0, 2 * Math.PI);
     canvasContext.fill();
+
+    // pacman finished version
+    // canvasContext.beginPath();
+    // canvasContext.fillStyle = 'yellow';
+    // canvasContext.arc(200, 300, 20, 0.1 * Math.PI, 1.85 * Math.PI);
+    // canvasContext.lineTo(200, 300);
+    // canvasContext.fill();
 }
