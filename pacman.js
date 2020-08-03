3bd37359ab10fced0c29e88a74f9901ce90046cd
MarijnWijb <m.wijbenga@outlook.com>
2020-08-03 12:31:31 +0200

    inserted canvas js

--- pacman.js
+++ pacman.js
@@ -12,43 +12,567 @@ window.onload = function() {
 
 function drawMaze() {
     canvasContext.fillStyle = 'black';
-    // canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
-    canvasContext.fillStyle = strokeColor;
+    canvasContext.save();
+    canvasContext.beginPath();
+    canvasContext.moveTo(0.0, 40.0);
+    canvasContext.lineTo(800.0, 40.0);
+    canvasContext.lineTo(800.0, 0.0);
+    canvasContext.lineTo(0.0, 0.0);
+    canvasContext.lineTo(0.0, 40.0);
+    canvasContext.closePath();
+    canvasContext.fillStyle = "darkblue";
+    canvasContext.fill("evenodd");
 
-    // outer lines
-    canvasContext.fillRect(0, 0, canvas.width, stroke);
-    canvasContext.fillRect(0, 0, stroke, 240 );
-    canvasContext.fillRect(0, 0, stroke, 240 );
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(0.0, 280.0);
+    canvasContext.lineTo(40.0, 280.0);
+    canvasContext.lineTo(40.0, 0.0);
+    canvasContext.lineTo(0.0, 0.0);
+    canvasContext.lineTo(0.0, 280.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
 
-    // blocks left top
-    canvasContext.fillRect(80, 80, 80, stroke );
-    canvasContext.fillRect(240, 80, 80, stroke );
-    canvasContext.fillRect(80, 160, 80, stroke );
-    canvasContext.fillRect(240, 160, stroke, 160 );
-    canvasContext.fillRect(240, 230, 80, stroke );
-    canvasContext.fillRect(240, 230, 80, stroke );
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(760.0, 280.0);
+    canvasContext.lineTo(800.0, 280.0);
+    canvasContext.lineTo(800.0, 0.0);
+    canvasContext.lineTo(760.0, 0.0);
+    canvasContext.lineTo(760.0, 280.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
 
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(0.0, 280.0);
+    canvasContext.lineTo(160.0, 280.0);
+    canvasContext.lineTo(160.0, 240.0);
+    canvasContext.lineTo(0.0, 240.0);
+    canvasContext.lineTo(0.0, 280.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
 
-    // canvasContext.fillRect();
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(120.0, 360.0);
+    canvasContext.lineTo(160.0, 360.0);
+    canvasContext.lineTo(160.0, 240.0);
+    canvasContext.lineTo(120.0, 240.0);
+    canvasContext.lineTo(120.0, 360.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
 
-    // center line
-    canvasContext.fillRect(390, 0, stroke, 800 );
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(0.0, 360.0);
+    canvasContext.lineTo(120.0, 360.0);
+    canvasContext.lineTo(120.0, 320.0);
+    canvasContext.lineTo(0.0, 320.0);
+    canvasContext.lineTo(0.0, 360.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
 
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(640.0, 280.0);
+    canvasContext.lineTo(800.0, 280.0);
+    canvasContext.lineTo(800.0, 240.0);
+    canvasContext.lineTo(640.0, 240.0);
+    canvasContext.lineTo(640.0, 280.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(640.0, 360.0);
+    canvasContext.lineTo(680.0, 360.0);
+    canvasContext.lineTo(680.0, 280.0);
+    canvasContext.lineTo(640.0, 280.0);
+    canvasContext.lineTo(640.0, 360.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(680.0, 360.0);
+    canvasContext.lineTo(800.0, 360.0);
+    canvasContext.lineTo(800.0, 320.0);
+    canvasContext.lineTo(680.0, 320.0);
+    canvasContext.lineTo(680.0, 360.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(640.0, 120.0);
+    canvasContext.lineTo(720.0, 120.0);
+    canvasContext.lineTo(720.0, 80.0);
+    canvasContext.lineTo(640.0, 80.0);
+    canvasContext.lineTo(640.0, 120.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(80.0, 120.0);
+    canvasContext.lineTo(160.0, 120.0);
+    canvasContext.lineTo(160.0, 80.0);
+    canvasContext.lineTo(80.0, 80.0);
+    canvasContext.lineTo(80.0, 120.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(200.0, 120.0);
+    canvasContext.lineTo(320.0, 120.0);
+    canvasContext.lineTo(320.0, 80.0);
+    canvasContext.lineTo(200.0, 80.0);
+    canvasContext.lineTo(200.0, 120.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(360.0, 120.0);
+    canvasContext.lineTo(440.0, 120.0);
+    canvasContext.lineTo(440.0, 0.0);
+    canvasContext.lineTo(360.0, 0.0);
+    canvasContext.lineTo(360.0, 120.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(480.0, 120.0);
+    canvasContext.lineTo(600.0, 120.0);
+    canvasContext.lineTo(600.0, 80.0);
+    canvasContext.lineTo(480.0, 80.0);
+    canvasContext.lineTo(480.0, 120.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(80.0, 200.0);
+    canvasContext.lineTo(160.0, 200.0);
+    canvasContext.lineTo(160.0, 160.0);
+    canvasContext.lineTo(80.0, 160.0);
+    canvasContext.lineTo(80.0, 200.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(200.0, 360.0);
+    canvasContext.lineTo(240.0, 360.0);
+    canvasContext.lineTo(240.0, 160.0);
+    canvasContext.lineTo(200.0, 160.0);
+    canvasContext.lineTo(200.0, 360.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(240.0, 280.0);
+    canvasContext.lineTo(320.0, 280.0);
+    canvasContext.lineTo(320.0, 240.0);
+    canvasContext.lineTo(240.0, 240.0);
+    canvasContext.lineTo(240.0, 280.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(560.0, 360.0);
+    canvasContext.lineTo(600.0, 360.0);
+    canvasContext.lineTo(600.0, 160.0);
+    canvasContext.lineTo(560.0, 160.0);
+    canvasContext.lineTo(560.0, 360.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(480.0, 280.0);
+    canvasContext.lineTo(560.0, 280.0);
+    canvasContext.lineTo(560.0, 240.0);
+    canvasContext.lineTo(480.0, 240.0);
+    canvasContext.lineTo(480.0, 280.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(280.0, 200.0);
+    canvasContext.lineTo(520.0, 200.0);
+    canvasContext.lineTo(520.0, 160.0);
+    canvasContext.lineTo(280.0, 160.0);
+    canvasContext.lineTo(280.0, 200.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(200.0, 720.0);
+    canvasContext.lineTo(240.0, 720.0);
+    canvasContext.lineTo(240.0, 640.0);
+    canvasContext.lineTo(200.0, 640.0);
+    canvasContext.lineTo(200.0, 720.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(80.0, 760.0);
+    canvasContext.lineTo(320.0, 760.0);
+    canvasContext.lineTo(320.0, 720.0);
+    canvasContext.lineTo(80.0, 720.0);
+    canvasContext.lineTo(80.0, 760.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(360.0, 600.0);
+    canvasContext.lineTo(440.0, 600.0);
+    canvasContext.lineTo(440.0, 520.0);
+    canvasContext.lineTo(360.0, 520.0);
+    canvasContext.lineTo(360.0, 600.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(360.0, 760.0);
+    canvasContext.lineTo(440.0, 760.0);
+    canvasContext.lineTo(440.0, 680.0);
+    canvasContext.lineTo(360.0, 680.0);
+    canvasContext.lineTo(360.0, 760.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(280.0, 680.0);
+    canvasContext.lineTo(520.0, 680.0);
+    canvasContext.lineTo(520.0, 640.0);
+    canvasContext.lineTo(280.0, 640.0);
+    canvasContext.lineTo(280.0, 680.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(360.0, 280.0);
+    canvasContext.lineTo(440.0, 280.0);
+    canvasContext.lineTo(440.0, 200.0);
+    canvasContext.lineTo(360.0, 200.0);
+    canvasContext.lineTo(360.0, 280.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(0.0, 442.1);
+    canvasContext.lineTo(160.0, 442.1);
+    canvasContext.lineTo(160.0, 400.0);
+    canvasContext.lineTo(0.0, 400.0);
+    canvasContext.lineTo(0.0, 442.1);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(120.0, 520.0);
+    canvasContext.lineTo(160.0, 520.0);
+    canvasContext.lineTo(160.0, 442.1);
+    canvasContext.lineTo(120.0, 442.1);
+    canvasContext.lineTo(120.0, 520.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(0.0, 520.0);
+    canvasContext.lineTo(120.0, 520.0);
+    canvasContext.lineTo(120.0, 480.0);
+    canvasContext.lineTo(0.0, 480.0);
+    canvasContext.lineTo(0.0, 520.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(200.0, 520.0);
+    canvasContext.lineTo(240.0, 520.0);
+    canvasContext.lineTo(240.0, 400.0);
+    canvasContext.lineTo(200.0, 400.0);
+    canvasContext.lineTo(200.0, 520.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(560.0, 520.0);
+    canvasContext.lineTo(600.0, 520.0);
+    canvasContext.lineTo(600.0, 400.0);
+    canvasContext.lineTo(560.0, 400.0);
+    canvasContext.lineTo(560.0, 520.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(0.0, 800.0);
+    canvasContext.lineTo(40.0, 800.0);
+    canvasContext.lineTo(40.0, 520.0);
+    canvasContext.lineTo(0.0, 520.0);
+    canvasContext.lineTo(0.0, 800.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(0.0, 840.0);
+    canvasContext.lineTo(800.0, 840.0);
+    canvasContext.lineTo(800.0, 800.0);
+    canvasContext.lineTo(0.0, 800.0);
+    canvasContext.lineTo(0.0, 840.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(640.0, 200.0);
+    canvasContext.lineTo(720.0, 200.0);
+    canvasContext.lineTo(720.0, 160.0);
+    canvasContext.lineTo(640.0, 160.0);
+    canvasContext.lineTo(640.0, 200.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(360.0, 350.0);
+    canvasContext.lineTo(440.0, 350.0);
+    canvasContext.lineTo(440.0, 330.0);
+    canvasContext.lineTo(360.0, 330.0);
+    canvasContext.lineTo(360.0, 350.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(200.0, 600.0);
+    canvasContext.lineTo(320.0, 600.0);
+    canvasContext.lineTo(320.0, 560.0);
+    canvasContext.lineTo(200.0, 560.0);
+    canvasContext.lineTo(200.0, 600.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(480.0, 600.0);
+    canvasContext.lineTo(600.0, 600.0);
+    canvasContext.lineTo(600.0, 560.0);
+    canvasContext.lineTo(480.0, 560.0);
+    canvasContext.lineTo(480.0, 600.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(280.0, 440.0);
+    canvasContext.lineTo(320.0, 440.0);
+    canvasContext.lineTo(320.0, 320.0);
+    canvasContext.lineTo(280.0, 320.0);
+    canvasContext.lineTo(280.0, 440.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(480.0, 440.0);
+    canvasContext.lineTo(520.0, 440.0);
+    canvasContext.lineTo(520.0, 320.0);
+    canvasContext.lineTo(480.0, 320.0);
+    canvasContext.lineTo(480.0, 440.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(280.0, 520.0);
+    canvasContext.lineTo(520.0, 520.0);
+    canvasContext.lineTo(520.0, 480.0);
+    canvasContext.lineTo(280.0, 480.0);
+    canvasContext.lineTo(280.0, 520.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(320.0, 440.0);
+    canvasContext.lineTo(480.0, 440.0);
+    canvasContext.lineTo(480.0, 400.0);
+    canvasContext.lineTo(320.0, 400.0);
+    canvasContext.lineTo(320.0, 440.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(320.0, 360.0);
+    canvasContext.lineTo(360.0, 360.0);
+    canvasContext.lineTo(360.0, 320.0);
+    canvasContext.lineTo(320.0, 320.0);
+    canvasContext.lineTo(320.0, 360.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(440.0, 360.0);
+    canvasContext.lineTo(480.0, 360.0);
+    canvasContext.lineTo(480.0, 320.0);
+    canvasContext.lineTo(440.0, 320.0);
+    canvasContext.lineTo(440.0, 360.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(80.0, 600.0);
+    canvasContext.lineTo(160.0, 600.0);
+    canvasContext.lineTo(160.0, 560.0);
+    canvasContext.lineTo(80.0, 560.0);
+    canvasContext.lineTo(80.0, 600.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(120.0, 680.0);
+    canvasContext.lineTo(160.0, 680.0);
+    canvasContext.lineTo(160.0, 600.0);
+    canvasContext.lineTo(120.0, 600.0);
+    canvasContext.lineTo(120.0, 680.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(40.0, 680.0);
+    canvasContext.lineTo(80.0, 680.0);
+    canvasContext.lineTo(80.0, 640.0);
+    canvasContext.lineTo(40.0, 640.0);
+    canvasContext.lineTo(40.0, 680.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(560.0, 720.0);
+    canvasContext.lineTo(600.0, 720.0);
+    canvasContext.lineTo(600.0, 640.0);
+    canvasContext.lineTo(560.0, 640.0);
+    canvasContext.lineTo(560.0, 720.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(480.0, 760.0);
+    canvasContext.lineTo(720.0, 760.0);
+    canvasContext.lineTo(720.0, 720.0);
+    canvasContext.lineTo(480.0, 720.0);
+    canvasContext.lineTo(480.0, 760.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(640.0, 440.0);
+    canvasContext.lineTo(800.0, 440.0);
+    canvasContext.lineTo(800.0, 400.0);
+    canvasContext.lineTo(640.0, 400.0);
+    canvasContext.lineTo(640.0, 440.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(640.0, 520.0);
+    canvasContext.lineTo(680.0, 520.0);
+    canvasContext.lineTo(680.0, 440.0);
+    canvasContext.lineTo(640.0, 440.0);
+    canvasContext.lineTo(640.0, 520.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(680.0, 520.0);
+    canvasContext.lineTo(800.0, 520.0);
+    canvasContext.lineTo(800.0, 480.0);
+    canvasContext.lineTo(680.0, 480.0);
+    canvasContext.lineTo(680.0, 520.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(760.0, 800.0);
+    canvasContext.lineTo(800.0, 800.0);
+    canvasContext.lineTo(800.0, 520.0);
+    canvasContext.lineTo(760.0, 520.0);
+    canvasContext.lineTo(760.0, 800.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(640.0, 600.0);
+    canvasContext.lineTo(720.0, 600.0);
+    canvasContext.lineTo(720.0, 560.0);
+    canvasContext.lineTo(640.0, 560.0);
+    canvasContext.lineTo(640.0, 600.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(640.0, 680.0);
+    canvasContext.lineTo(680.0, 680.0);
+    canvasContext.lineTo(680.0, 600.0);
+    canvasContext.lineTo(640.0, 600.0);
+    canvasContext.lineTo(640.0, 680.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+
+    // layer1/Path
+    canvasContext.beginPath();
+    canvasContext.moveTo(720.0, 680.0);
+    canvasContext.lineTo(760.0, 680.0);
+    canvasContext.lineTo(760.0, 640.0);
+    canvasContext.lineTo(720.0, 640.0);
+    canvasContext.lineTo(720.0, 680.0);
+    canvasContext.closePath();
+    canvasContext.fill("evenodd");
+    canvasContext.restore();
 
 }
 
 function drawPacman() {
-    canvasContext.fillStyle = 'yellow';
-    canvasContext.arc(50, 90, 20, 0, 2 * Math.PI);
-    canvasContext.arc(360, 90, 20, 0, 2 * Math.PI);
-    canvasContext.arc(200, 90, 20, 0, 2 * Math.PI);
-    canvasContext.fill();
 
-    // pacman finished version
-    // canvasContext.beginPath();
-    // canvasContext.fillStyle = 'yellow';
-    // canvasContext.arc(200, 300, 20, 0.1 * Math.PI, 1.85 * Math.PI);
-    // canvasContext.lineTo(200, 300);
-    // canvasContext.fill();
+    canvasContext.beginPath();
+    canvasContext.fillStyle = 'yellow';
+    canvasContext.arc(180, 300, 15, 0.1 * Math.PI, 1.85 * Math.PI);
+    canvasContext.lineTo(180, 300);
+    canvasContext.fill();
 }
