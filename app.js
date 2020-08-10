a2b056255a0638a29103c7710ef9d06031399c03
marijnwijbenga <m.wijbenga@outlook.com>
2020-08-10 14:08:02 +0200

    deleted everything and start anew

--- app.js
+++ app.js
@@ -1,43 +1,7 @@
-import Controller from './lib/controller.js';
-import PacMan from './lib/pacman.js';
-import Painter from './lib/painter.js';
+// todo learn about OOP, classes & .this
+    // ES6 J5 Youtube
+    // ES6 OOP Crashcourse Youtube
 
-let context;
-let controller;
-
-window.onload = function () {
-    const canvas = document.getElementById('game');
-    context = canvas.getContext('2d');
-
-    initialize();
-    document.addEventListener('keydown', (event) => {
-       controller.onKeyDown(event.key);
-    });
-    setInterval(update, 1); // todo add speed const
-
-}
-
-function initialize() {
-    const painter = new Painter(context);
-    painter.paintMaze();
-
-    const pacMan = new PacMan();
-    painter.paintPacman(pacMan);
-
-    controller = new Controller();
-
-}
-function update() {
-    // todo paint pacman
-    // todo move pacman with controller
-    // todo paint bite animation
-    // todo repaint previous pacman area
-}
-
-
-
-
-
-
-
+// todo make a good looking pacman game page
+    // todo design first in affinity designer
 
