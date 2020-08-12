3bd90ac555892027bf7b7135ed6f377305850a30
MarijnWijb <m.wijbenga@outlook.com>
2020-08-12 17:37:34 +0200

    started anew

--- app.js
+++
@@ -1,7 +0,0 @@
-// todo learn about OOP, classes & .this
-    // ES6 J5 Youtube
-    // ES6 OOP Crashcourse Youtube
-
-// todo make a good looking pacman game page
-    // todo design first in affinity designer
-
